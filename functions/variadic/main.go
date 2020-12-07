8c004518b855fc765f56edd404a35ea34c6b58ca
Hemant singh <info90.hemant@gmail.com>
2020-12-07 19:12:12 +0530

    Variadic Function Concept
     Variadic function can use multiple parameters

---
+++ functions/variadic/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+func joinstr(element...string) string {
+	return strings.Join(element, "-")
+}
+
+func main() {
+	// Variadic function to joun strings
+	fmt.Println(joinstr())
+
+	// mutiple arguments
+	fmt.Println(joinstr("Geek","Geeks"))
+	fmt.Println(joinstr("Geek","for","Geeks"))
+	fmt.Println(joinstr("G","E","E","k","s"))
+}
