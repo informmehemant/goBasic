d22555006010422318754e02f8b6d8e05ec65312
Hemant singh <info90.hemant@gmail.com>
2020-12-16 08:54:22 +0530

    int func
    init function concept added

---
+++ functions/initFunction/main.go
@@ -0,0 +1,15 @@
+
+package main 
+
+import "fmt"
+
+func init() {
+	fmt.Println("Welcome to init() function")
+}
+
+func init() {
+	fmt.Println("Hello! init() function")
+}
+func main() {
+	fmt.Println("Welcome to main() fucntion ")
+}
